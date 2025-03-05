432c0672a622ae27ecad60c911e21c03f02c64b9
Saumya Vatsa <saumyavatsa2108@gmail.com>
2025-03-05 23:22:27 +0530

    Linked List implementation code

---
+++ LL-implement.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    public:
+    int data;
+    Node* next;
+
+    Node (int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+
+};
+
+int main(){
+    Node* head = nullptr;
+    Node* current = nullptr;
+
+    for (int i=0; i <5; i++){
+        int x;
+        cout << "Enter a value: ";
+        cin >>x;
+        Node* newNode = new Node(x, nullptr);
+
+        if(head == nullptr){
+            head = newNode;
+        }else{
+            current->next = newNode;
+        }
+
+        current = newNode;
+
+    }
+    
+    current = head; 
+    while (current != nullptr){
+        cout << "Value: " << current->data <<"\n";
+        current = current->next;
+    }
+}
